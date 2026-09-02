17b4d33830517ea8a1622302bd86db75a30987a2
7^6 <lhw.1012@gmail.com>
2026-09-02 14:16:29 +0800

    chore: release 2.12.2

--- pack.ps1
+++ pack.ps1
@@ -1,3 +1,3 @@
 Remove-Item -Path .\addon.xpi -ErrorAction SilentlyContinue
-Get-ChildItem -Path .\ -Exclude pack.ps1,*.xpi | Compress-Archive -CompressionLevel NoCompression -DestinationPath addon
+Get-ChildItem -Path .\ -Exclude .git,pack.ps1,addon.zip,*.xpi | Compress-Archive -CompressionLevel NoCompression -DestinationPath addon
 Rename-Item -Path .\addon.zip -NewName addon.xpi
